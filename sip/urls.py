a9fc9c5a1e2d51e4eeb0dba0780867eb84062d61
jallisson <jallissonn@hotmail.com>
2019-05-20 09:28:23 -0300

    acrescentando o favicon

--- sip/urls.py
+++ sip/urls.py
@@ -7,10 +7,13 @@ from . import views
 #from .views import VendaDetail
 #from .views import RelatorioListView
 #from .views import RelatorioDetail
+#favicon_view = RedirectView.as_view(url='/static/img/favicon.ico', permanent=True)
+
 
 urlpatterns = [
     #path('', views.index, name='index'),
     #url(r'^venda/(?P<pk>\d+)/$', VendaDetail.as_view(), name='venda_detail'),
 	#url(r'^lista/(?P<pk>\d+)/$', RelatorioListView.as_view(), name='venda_list'),
+	#re_path(r'^favicon\.ico$', favicon_view),
 	#url(r'^relatorio/(?P<pk>\d+)/$', RelatorioDetail.as_view(), name='venda_data_list'),
 ]
